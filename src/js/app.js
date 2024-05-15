7791ceea5d01838cddd88c4e15825925eda5a497
David Martínez <dave.7u7@gmail.com>
2024-05-15 03:05:13 -0600

    Leer las preferencias del sistema

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMorde(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark');
+
+    // console.log(prefiereDarkMode.matches);
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
