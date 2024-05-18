d0c8dc8f14ee0b10c2097e4b8bbbf243ce3868a2
David Martínez <dave.7u7@gmail.com>
2024-05-17 20:57:51 -0600

    Mostrar una alerta cuando una propiedad fue creada

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+    $resultado = $_GET['resultado'] ?? null;
 
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -7,6 +8,11 @@
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raíces</h1>
 
+        <?php if(intval($resultado) === 1): ?>
+            <p class="alerta exito">Anuncio creado correctamente</p>
+        <?php endif; ?>
+
+        
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
     </main>
 
