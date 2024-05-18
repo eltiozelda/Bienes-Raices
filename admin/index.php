2f9650fed8981913cca477dffb31355b9be828da
David Martínez <dave.7u7@gmail.com>
2024-05-17 21:06:17 -0600

    Creando una tabla para mostrar las propiedades de la base de datos

--- admin/index.php
+++ admin/index.php
@@ -12,8 +12,30 @@
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
 
-        
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/imagenes/48436e7036f0943d5fd7e1dc82fa103a.jpg" class="imagen-tabla"></td>
+                    <td>$120000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
     <?php
