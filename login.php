4d54051fd435e08e46d2947f77b11dcffafc5320
David Martínez <dave.7u7@gmail.com>
2024-05-20 02:20:20 -0600

    Validaciones al formulario

--- login.php
+++ login.php
@@ -1,5 +1,33 @@
 <?php
 
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    $errores = [];
+    // Autenticar el usuario
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        echo "<pre>";
+        var_dump(($_POST));
+        echo "</pre>";
+        
+        $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if(!$email) {
+            $errores[] = "El email es obligatorio o no es válido";
+        }
+
+        if(!$password) {
+            $errores[] = "El password es obligatorio o no es válido";
+        }
+
+        echo "<pre>";
+        var_dump(($errores));
+        echo "</pre>";
+    }
+
+    // Incluye el header
     require 'includes/funciones.php';
 
     incluirTemplate('header');
@@ -8,17 +36,22 @@
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar sesión</h1>
 
-        <form class="formulario">
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+        <form method="POST" class="formulario">
             <legend>Email y Passwort</legend>
 
             <fieldset>
                 <legend>Información Personal</legend>
 
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu E-mail" id="email">
+                <input type="email" name="email" placeholder="Tu E-mail" id="email" required>
 
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu password" id="password">
+                <input type="password" name="password" placeholder="Tu password" id="password" required>
             </fieldset>
 
             <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
